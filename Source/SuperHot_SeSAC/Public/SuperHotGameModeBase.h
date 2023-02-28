440d42633d561c354ac9f0da1b01a30e5e893c72
TGLEEEE <21bjh21@gmail.com>
2023-02-28 15:08:46 +0900

    230228 게임모드베이스 추가

---
+++ Source/SuperHot_SeSAC/Public/SuperHotGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SuperHotGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUPERHOT_SESAC_API ASuperHotGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/SuperHot_SeSAC/SuperHot_SeSAC.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SuperHot_SeSAC.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SuperHot_SeSAC, "SuperHot_SeSAC" );
