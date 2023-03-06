8861365552df8ee8ab82748eeb07aebc259855aa
TGLEEEE <21bjh21@gmail.com>
2023-03-06 19:20:56 +0900

    230306 리버트

--- Source/SuperHot_SeSAC/Private/Pistol.cpp
+++ Source/SuperHot_SeSAC/Private/Pistol.cpp
@@ -14,7 +14,23 @@ void APistol::BeginPlay()
 void APistol::Fire()
 {
 	// 1초동안 시간 정상적으로 흐르도록
+<<<<<<< HEAD
 
+=======
+	GetWorldTimerManager().ClearTimer(resetTimer);
+	GetWorldTimerManager().SetTimer(resetTimer, FTimerDelegate::CreateLambda([&]()
+	{
+		UGameplayStatics::SetGlobalTimeDilation(GetWorld(), 0.001);
+		if(player)
+		{
+			player->bIsFiring = false;	
+		}
+		UE_LOG(LogTemp, Warning, TEXT("timerDOne"));
+	}), 1, false);
+	
+	UGameplayStatics::SetGlobalTimeDilation(GetWorld(),1);
+	
+>>>>>>> parent of 795060b (230306 오른손 시점에 고정)
 	// 총알 액터 스폰
 	GetWorld()->SpawnActor<ABullet>(bulletFactory, weaponMesh->GetSocketTransform(TEXT("Front")));
 }
